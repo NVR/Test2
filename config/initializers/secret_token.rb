f2c2bcc268316bb5cbc733eea743b95e80cfdda2
NVR <envy.94@mail.ru>
2011-11-16 16:02:08 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test2::Application.config.secret_token = 'd10bef097310ecb153aeb5c544aad98ea98db7a23a01ffa99ad9ab8381b4739456dd3d2634f242a826e9a800ecde0cb92f66803130b778385bc7c737f25bd5f9'
