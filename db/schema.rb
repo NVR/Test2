8fb3014c74aeda1a750152bdca8f12d8d7bdb878
NVR <envy.94@mail.ru>
2011-11-17 23:03:18 +0300

    adde user_id to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111117091024) do
+ActiveRecord::Schema.define(:version => 20111117195958) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20111117091024) do
     t.integer  "repeat_after"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
