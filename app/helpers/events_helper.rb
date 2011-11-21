63d14be3b9dad645b7817ac395dead34c4f3ef3d
NVR <envy.94@mail.ru>
2011-11-21 16:56:12 +0300

    last commit on br

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -18,8 +18,8 @@ module EventsHelper
     # args is an argument hash containing :event, :day, and :options
     calendar event_calendar_opts do |args|
       event = args[:event]
-      %(#{ link_to event.name , event_path(event.id) })
       #peredat id eventa kak-to?
+      %(<a href="/events/#{event.id}" title="#{h(event.name)}" onclick="custom()">#{h(event.name)}</a>)
     end
   end
 end
